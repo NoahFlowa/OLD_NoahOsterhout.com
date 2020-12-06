7d277ff2a373f5495b22d018d6c66b35fa7c7ae2
NoahFlowa <osterhoutn@gmail.com>
2020-12-06 03:04:25 -0500

    Removed some items and added PHP function to get year

--- footer.php
+++ footer.php
@@ -12,12 +12,10 @@
                     <tr>
                         <td>Made with <i class="fas fa-code" aria-hidden="true" style="color: #22c2fc;"></i> in 6 Hours</td>
                         <td>Get In Touch <i class="fas fa-bullhorn" style="color: #22c2fc;"></i></td>
-                        <td>Find me <i class="fas fa-binoculars"  style="color: #22c2fc;"></i></td>
                     </tr>
                     <tr>
-                        <td style="color: #71767F">&copy; Noah Osterhout - 2018</td>
+                        <td style="color: #71767F">&copy; Noah Osterhout - <?php echo date("Y") ?></td>
                         <td style="color: #71767F"><a href="mailto:noah@noahosterhout.com">noah@noahosterhout.com</a></td>
-                        <td style="color: #71767F"><a href="https://twitter.com/NoahFlowa"><i class="fab fa-twitter ico-hover" aria-hidden="true"></i></a> | <a href="http://steamcommunity.com/id/NoahFlowa/"><i class="fab fa-steam ico-hover" aria-hidden="true"></i></a> | <a href="https://discord.gg/psDgrPG"><i class="fab fa-discord ico-hover" aria-hidden="true"></i></a></td>
                     </tr>
                 </tbody>
             </table>
