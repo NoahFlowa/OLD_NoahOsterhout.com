f848d79adc826169d0a7a8b90274843667c47270
NoahFlowa <osterhoutn@gmail.com>
2020-12-06 03:03:45 -0500

    Updated Experience with last 2 years of info

--- experience.php
+++ experience.php
@@ -12,34 +12,46 @@
                         
                         <tr>
                             <td style="color: #71767F">2017 - Present</td>
-                            <td>Full-Time Student at Northwestern Michigan College for Computer Information Systems</td>
+                            <td>Full-Time Student at Northwestern Michigan College and Ferris State University for Computer Information Systems as well as CEO at <a href="https://osterhoutklein.com">OK Enterprises</a></td>
                         </tr>
                         <tr>
-                            <td style="color: #71767F">March 2018 </td>
-                            <td>Became a member of <a href="https://www.ptk.org" style="color: #22c2fc;">Phi Theta Kappa Honor Society</a>  [ Alpha Rho PI Member ]</td>
+                            <td style="color: #71767F">2020 </td>
+                            <td>Changed <a href="https://osterhoutklein.com">OK Enterprises</a> to offer Business services such as IT, Software Development, Business Management, Marketing and more.  Founded <a href="https://osterhoutklein.org">OK Foundations</a> with Mark Klein to offer free services in light of Coronavirus.  Started Ferris University for Computer Information Systems.</td>
                         </tr>
                         <tr>
-                            <td style="color: #71767F">February 2018 </td>
+                            <td style="color: #71767F">2019</td>
+                            <td>Founded <a href="https://osterhoutklein.com">OK Enterprises</a> with friend, Mark Klein, to be an Umbrella Company for various ventures.</td>
+                        </tr>
+                        <tr>
+                            <td style="color: #71767F">May 2018 </td>
+                            <td>Obtained Computer Studies - Assitant Developer Certificate from Northwestern Michigan College</td>
+                        </tr>
+                        <tr>
+                            <td style="color: #71767F">2018</td>
                             <td>Acquired <a href="https://www.youracclaim.com/badges/cfd61f0f-2672-4491-87f9-19b7af2abf13" style="color: #22c2fc;">MTA Certification for Database Fundamentals</a> through Microsoft</td>
                         </tr>
                         <tr>
-                            <td style="color: #71767F">May 2017</td>
-                            <td>Web Designer | Content Manager for Front Street Writers at Traverse City Career-Tech Center</td>
+                            <td style="color: #71767F">2017 </td>
+                            <td>Became a member of <a href="https://www.ptk.org" style="color: #22c2fc;">Phi Theta Kappa Honor Society</a>  [ Alpha Rho PI Member ]</td>
                         </tr>
                         <tr>
                             <td style="color: #71767F">2016 - 2017</td>
-                            <td>Advisor to the Board for Web And Game Programming at Traverse City Career-Tech Center</td>
-                        </tr>
-                        <tr>
-                            <td style="color: #71767F">November - December 2016</td>
-                            <td>Web Designer | Content Manager Intern at AlpineWeb</td>
+                            <td>Advisor to the Board for Web And Game Programming as well as a Student at Traverse City Career-Tech Center</td>
                         </tr>
                         <tr>
                             <td style="color: #71767F">2015 - 2017</td>
                             <td>Student enrolled in Web and Game Programming at Traverse City Career-Tech Center</td>
                         </tr>
                         <tr>
-                            <td style="color: #71767F">April 2012</td>
+                            <td style="color: #71767F">2016</td>
+                            <td>Web Designer | Content Manager Intern at AlpineWeb</td>
+                        </tr>
+                        <tr>
+                            <td style="color: #71767F">2012 - 2015</td>
+                            <td>Learning new technologies and deciding to work in Computers</td>
+                        </tr>
+                        <tr>
+                            <td style="color: #71767F">2012</td>
                             <td>Contract Web Designer at ImpactFolios</td>
                         </tr>
                     </tbody>
