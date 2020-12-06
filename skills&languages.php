305299b05f6bafa0ad808dcb749b646c1392215d
NoahFlowa <osterhoutn@gmail.com>
2020-12-06 03:04:56 -0500

    Add more skills and languages.  Removed some that I havent used in ages

--- skills&languages.php
+++ skills&languages.php
@@ -15,11 +15,11 @@
                         </tr>
                         <tr>
                             <td>Web Libraries | Frameworks | Libraries</td>
-                            <td style="color: #71767F">Bootstrap 4 | JQuery | Angular-JS|  Font Awesome | EmguCV</td>
+                            <td style="color: #71767F">Bootstrap 4 | JQuery | Angular |  Font Awesome | Vue | React | Flutter</td>
                         </tr>
                         <tr>
                             <td>Object-Oriented Programming Languages</td>
-                            <td style="color: #71767F">Python | C# | C++ | Swift | Arduino C</td>
+                            <td style="color: #71767F">Python | C# | C++ | Swift | Java | Dart | Kotlin | TypeScript</td>
                         </tr>
                         <tr>
                             <td>Database Stuff | Data Inerchange Languages</td>
@@ -27,15 +27,15 @@
                         </tr>
                         <tr>
                             <td>Certifications</td>
-                            <td style="color: #71767F"><a href="https://www.youracclaim.com/badges/cfd61f0f-2672-4491-87f9-19b7af2abf13" style="color: #22c2fc;">Database Fundamentals</a></td>
+                            <td style="color: #71767F"><a href="https://www.youracclaim.com/badges/cfd61f0f-2672-4491-87f9-19b7af2abf13" style="color: #22c2fc;">Database Fundamentals</a><br>Computer Studies - Assitant Developer Certificate from Northwestern Michigan College</td>
                         </tr>
                         <tr>
                             <td>Applications | Enviroments</td>
-                            <td style="color: #71767F">Visual Studio Editors | XCode | Unity | Unreal | SSMS | DevServer | Photoshop | WordPress CMS | PHP My Admin</td>
+                            <td style="color: #71767F">Visual Studio Editors | XCode | Unity | Unreal | SSMS | DevServer | Photoshop | WordPress CMS | PHP My Admin | Docker</td>
                         </tr>
                         <tr>
                             <td>Command Line Interfaces</td>
-                            <td style="color: #71767F">Unix Terminal Commands | Windows CMD Prompt | Windows Powershell | Linux Subsystem for Windows</td>
+                            <td style="color: #71767F">Unix Terminal Commands | Windows CMD Prompt | Windows Powershell | Linux Subsystem for Windows | NPM | Flutter</td>
                         </tr>
                         <tr>
                             <td>Utilities</td>
